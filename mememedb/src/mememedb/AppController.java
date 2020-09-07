9598abfff70cee9f347798d0c7da4c72b91be41a
Magne Halvorsen <mh95244719@gmail.com>
2020-09-07 15:46:49 +0200

    added fxml and controller

---
+++ mememedb/src/mememedb/AppController.java
@@ -0,0 +1,36 @@
+package mememedb;
+
+//Import javafx stuff
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.event.EventHandler;
+
+//Java Utils
+import java.util.HashMap;
+import java.text.DecimalFormat;
+
+public class AppController
+{
+  // @FXML
+  // Button clickMeButton;
+
+  // @FXML
+  // void handleClickMeButtonAction()
+  // {
+  //   clickMeButton.setText("Thanks!");
+  // }
+
+  //Initialize
+  @FXML
+  public void initialize()
+  {
+    //Do stuff
+  }
+}
