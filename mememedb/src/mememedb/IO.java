e70854acc90ff3b30d2e0a9c075de6ff02a5d418
jostbak <jostbak@stud.ntnu.no>
2020-09-16 14:11:12 +0200

    added throws to Interface

--- mememedb/src/mememedb/IO.java
+++ mememedb/src/mememedb/IO.java
@@ -1,13 +1,14 @@
 package mememedb;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public interface IO {
 	
 	
 	public List<Post> getPostList();
-	public void savePost(Post post);
-	public void saveImage(File image);
+	public void savePost(Post post) throws IOException;
+	public void saveImage(File image) throws IOException;
 	
 }
