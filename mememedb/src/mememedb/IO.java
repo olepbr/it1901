51d44d6376ce723558ebdbb0a7049ec3d79a2f01
jostbak <jostbak@stud.ntnu.no>
2020-09-16 14:04:00 +0200

    filled simple IO interface

--- mememedb/src/mememedb/IO.java
+++ mememedb/src/mememedb/IO.java
@@ -1,5 +1,13 @@
 package mememedb;
 
-public interface IO {
+import java.io.File;
+import java.util.List;
 
+public interface IO {
+	
+	
+	public List<Post> getPostList();
+	public void savePost(Post post);
+	public void saveImage(File image);
+	
 }
