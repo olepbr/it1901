28f28d1ce4999e7491bf585c31970bdf81574e70
jostbak <jostbak@stud.ntnu.no>
2020-09-28 11:35:37 +0200

    Updated AppTest to test login and register interface

--- mememedb/test/mememedb/AppTest.java
+++ mememedb/test/mememedb/AppTest.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.Node;
@@ -30,11 +31,35 @@ public class AppTest extends ApplicationTest {
   }
 
   @Test
-  public void testButtons() {
-    final Button logoutButton = (Button) parent.lookup("#logout");
-    final Button addContent = (Button) parent.lookup("#addContent");
-    clickOn(addContent);
-    clickOn(logoutButton);
+  public void testLogin() {
+    final TextField loginEmail = (TextField) parent.lookup("#loginEmailText");
+    final TextField loginPassword = (TextField) parent.lookup("#loginPassText");
+    final Button loginButton = (Button) parent.lookup("#loginButton");
+    clickOn(loginEmail);
+    write("cmail@gmail.com");
+    clickOn(loginPassword);
+    write("passw0rd123qwerty");
+    clickOn(loginButton);
+  }
+  
+  @Test
+  public void testRegister() {
+    final Button registerButton = (Button) parent.lookup("#registerButton");
+    final TextField regEmail = (TextField) parent.lookup("#emailTextField");
+    final TextField regPassword = (TextField) parent.lookup("#passwordTextField");
+    final TextField regUser = (TextField) parent.lookup("#usernameTextField");
+    final TextField regName = (TextField) parent.lookup("#nameTextField");
+    final Button createButton = (Button) parent.lookup("#createUserButton");
+    clickOn(registerButton);
+    clickOn(regName);
+    write("Testman Testson");
+    clickOn(regUser);
+    write("kewltester123");
+    clickOn(regEmail);
+    write("cmail@gmail.com");
+    clickOn(regPassword);
+    write("passw0rd123qwerty");
+    clickOn(createButton);
   }
 
 }
