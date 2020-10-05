9903da6f52c053a85b878e2303a95c024db510f5
Magne Halvorsen <maghalv@stud.ntnu.no>
2020-10-05 11:31:03 +0200

    Squashed more bugs

--- mememedb/core/src/core/datastructures/Database.java
+++ mememedb/core/src/core/datastructures/Database.java
@@ -1,28 +1,24 @@
 package core.datastructures;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import core.io.IO;
+import core.io.LocalIO;
+import core.json.UserListSerializer;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
-import core.io.IO;
-import core.io.LocalIO;
-import core.json.UserListSerializer;
-
-
 public class Database {
-
 
   @JsonSerialize(using = UserListSerializer.class)
   private List<User> users;
+
   private IO storage;
 
-
   /**
-   * Generates a new database object, Database contains all Users and Posts of the app,
-   * and corresponds with an IO-object to read and write data.
+   * Generates a new database object, Database contains all Users and Posts of the app, and
+   * corresponds with an IO-object to read and write data.
    */
   public Database() {
     storage = new LocalIO();
@@ -30,31 +26,25 @@ public class Database {
     reload();
   }
 
-
-  /**
-   * Removes cached database, and reloads from storage.
-   */
+  /** Removes cached database, and reloads from storage. */
   public void reload() {
     users = storage.getUserList();
   }
 
-  /**
-   * Saves cached database, overwriting previous data in storage.
-   */
+  /** Saves cached database, overwriting previous data in storage. */
   public void saveToStorage() {
     storage.save(users);
   }
 
   /**
-   * Saves post in database for user.
-   * Automatically updates storage.
+   * Saves post in database for user. Automatically updates storage.
    *
    * @param post The Post to save.
    * @param image The image belonging to the post
    * @param user Owner of the post.
    */
   public void savePost(Post post, File image, User user) throws IOException {
-    if(users.contains(user)) {
+    if (users.contains(user)) {
       users.add(user);
     }
     users.get(users.indexOf(user)).addPost(post);
@@ -63,13 +53,13 @@ public class Database {
   }
 
   /**
-   * Creates a new User in the database, unless the user already exists.
-   * Automatically updates storage.
+   * Creates a new User in the database, unless the user already exists. Automatically updates
+   * storage.
    *
    * @param user The User to save.
    */
   public void saveUser(User user) {
-    if(!users.contains(user)) {
+    if (!users.contains(user)) {
       users.add(user);
     }
     saveToStorage();
@@ -80,23 +70,22 @@ public class Database {
    *
    * @return Returns a list containing all posts.
    */
-  public List<Post> getPostList(){
+  public List<Post> getPostList() {
     List<Post> posts = new ArrayList<Post>();
-    for(User user : users) {
-      for(Post post : user.getPosts()) {
+    for (User user : users) {
+      for (Post post : user.getPosts()) {
         posts.add(post);
       }
     }
     return posts;
   }
 
-
   /**
    * Fetches a list of all users in the database
    *
    * @return returns a list of all users
    */
-  public List<User> getUsers(){
+  public List<User> getUsers() {
     return users;
   }
 
@@ -112,13 +101,15 @@ public class Database {
 
   /**
    * Attempts to find a user in the database with the given information.
+   *
    * @param username Username or email of the user
    * @param password The password of the User
    * @return User that logged on if it exists, null if no such user exists
    */
   public User tryLogin(String username, String password) {
-    for(User user : users) {
-      if((user.getEmail() == username || user.getNickname() == username) && user.getPassword() == password) {
+    for (User user : users) {
+      if ((user.getEmail().equals(username) || user.getNickname().equals(username))
+          && user.getPassword().equals(password)) {
         return user;
       }
     }
@@ -127,6 +118,7 @@ public class Database {
 
   /**
    * Checks if the input username already exists in the database.
+   *
    * @param username
    * @return true if the username exists in the database
    */
@@ -141,18 +133,12 @@ public class Database {
 
   /**
    * Makes a unique ID based on the amount of users that already exist in the database.
+   *
    * @return a new ID for a user being created.
    */
-  public int getNewID(){
-    int id = users.size(); id++;
+  public int getNewID() {
+    int id = users.size();
+    id++;
     return id;
   }
-
-
-
-
-
-
-
-
 }
--- mememedb/fxui/src/fxui/LoginController.java
+++ mememedb/fxui/src/fxui/LoginController.java
@@ -1,10 +1,7 @@
 package fxui;
 
-
+import core.datastructures.Database;
 import core.datastructures.User;
-import core.io.IO;
-import fxui.AppController;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -12,121 +9,108 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import core.datastructures.User;
-import core.datastructures.Database;
-import core.io.IO;
-
 import org.apache.commons.validator.routines.EmailValidator;
 
-import java.util.ArrayList;
+public class LoginController {
 
+  /**
+   * Controller class for the login- and register panes.
+   *
+   * @author Hanna Thevik
+   */
+  private Database database;
 
-public class LoginController{
+  private AppController parent;
+  // private User user;
+  // private ArrayList<User> users = new ArrayList<>();
 
-    /**
-     * Controller class for the login- and register panes.
-     *
-     * @author Hanna Thevik
-     */
+  // login
+  @FXML private AnchorPane loginAnchorPane;
+  @FXML private TextField loginUsername;
+  @FXML private PasswordField loginPasswordText;
+  @FXML private Button loginButton;
+  @FXML private Button registerButton;
+  @FXML private Label loginWarning;
 
-    private Database database;
-    private AppController parent;
-    private User user;
-    private ArrayList<User> users = new ArrayList<>();
+  // register
+  @FXML private AnchorPane registerAnchorPane;
+  @FXML private TextField nameTextField;
+  @FXML private TextField emailTextField;
+  @FXML private TextField usernameTextField;
+  @FXML private PasswordField passwordTextField;
+  @FXML private Button createUserButton;
+  @FXML private Label nameWarning;
+  @FXML private Label usernameWarning;
+  @FXML private Label emailWarning;
+  @FXML private Label passwordWarning;
 
+  public void setParent(AppController parent) {
+    this.parent = parent;
+  }
 
-    //login
-    @FXML private AnchorPane loginAnchorPane;
-    @FXML private TextField loginUsername;
-    @FXML private PasswordField loginPasswordText;
-    @FXML private Button loginButton;
-    @FXML private Button registerButton;
-    @FXML private Label loginWarning;
+  public void setDatabase(Database database) {
+    this.database = database;
+  }
 
-    //register
-    @FXML private AnchorPane registerAnchorPane;
-    @FXML private TextField nameTextField;
-    @FXML private TextField emailTextField;
-    @FXML private TextField usernameTextField;
-    @FXML private PasswordField passwordTextField;
-    @FXML private Button createUserButton;
-    @FXML private Label nameWarning;
-    @FXML private Label usernameWarning;
-    @FXML private Label emailWarning;
-    @FXML private Label passwordWarning;
+  /** Initializes the login and register panes. Hides the register pane. */
+  @FXML
+  private void initialize() {
+    loginAnchorPane.setVisible(true);
+    registerAnchorPane.setVisible(false);
+  }
 
-    public void setParent(AppController parent){
-        this.parent = parent;
+  /**
+   * Logs in a user when the correct email and password combo is used.
+   *
+   * @param event
+   */
+  @FXML
+  private void login(ActionEvent event) {
+    String username = loginUsername.getText();
+    String password = loginPasswordText.getText();
+    if (database.tryLogin(username, password) != null) {
+      parent.handleLogin(database.tryLogin(username, password));
+    } else {
+      loginWarning.setText("Password and email do not match!");
     }
-    
-    public void setDatabase(Database database) {
-      this.database = database;
+  }
+
+  /**
+   * Switches panes if the registerButton is clicked.
+   *
+   * @param e
+   */
+  @FXML
+  private void registerClick(ActionEvent e) {
+    loginAnchorPane.setVisible(false);
+    registerAnchorPane.setVisible(true);
+  }
+
+  /**
+   * Checks if all the strings in TextFields are valid.
+   *
+   * @param e
+   *     <p>Creates a new User object and saves it in database if all the fields are valid.
+   */
+  @FXML
+  private void createUser(ActionEvent e) {
+    String email = emailTextField.getText();
+    String name = nameTextField.getText();
+    String username = usernameTextField.getText();
+    String password = passwordTextField.getText();
+    if (name.isEmpty()) {
+      nameWarning.setText("Please put your full name");
+    } else if (!EmailValidator.getInstance().isValid(email)) {
+      emailWarning.setText("Please put a valid email address");
+    } else if (database.usernameExists(username)) {
+      usernameWarning.setText("Username is taken or not valid");
+    } else if (password.length() < 8) {
+      passwordWarning.setText("Password must contain at least 8 characters");
+    } else {
+      User user = new User(database.getNewID(), name, username, email);
+      user.setPassword(password);
+      database.saveUser(user);
+      parent.handleLogin(user);
     }
-
-
-    /**
-     * Initializes the login and register panes. Hides the register pane.
-     */
-    @FXML
-    private void initialize(){
-        loginAnchorPane.setVisible(true);
-        registerAnchorPane.setVisible(false);
-    }
-
-    /**
-     * Logs in a user when the correct email and password combo is used.
-     *
-     * @param event
-     */
-    @FXML
-    private void login(ActionEvent event){
-        String username = loginUsername.getText();
-        String password = loginPasswordText.getText();
-        if(database.tryLogin(username, password) != null){
-            parent.handleLogin(database.tryLogin(username, password));
-            }
-        else {
-            loginWarning.setText("Password and email do not match!");
-        }
-    }
-
-    /**
-     * Switches panes if the registerButton is clicked.
-     * @param e
-     */
-    @FXML
-    private void registerClick(ActionEvent e){
-        loginAnchorPane.setVisible(false);
-        registerAnchorPane.setVisible(true);
-    }
-
-
-    /**
-     * Checks if all the strings in TextFields are valid.
-     * @param e
-     *
-     * Creates a new User object and saves it in database if all the fields are valid.
-     */
-    @FXML
-    private void createUser(ActionEvent e){
-        String email = emailTextField.getText();
-        String name = nameTextField.getText();
-        String username = usernameTextField.getText();
-        String password = passwordTextField.getText();
-        if(name.isEmpty()) {
-            nameWarning.setText("Please put your full name");
-        } else if(!EmailValidator.getInstance().isValid(email)) {
-            emailWarning.setText("Please put a valid email address");
-        } else if(database.usernameExists(username)){
-            usernameWarning.setText("Username is taken or not valid");
-        } else if(password.length() < 8){
-            passwordWarning.setText("Password must contain at least 8 characters");
-        } else{
-            User user = new User(database.getNewID(), name, username, email);
-            user.setPassword(password);
-            database.saveUser(user);
-            parent.handleLogin(user);
-        }
-    }
-
+  }
 }
