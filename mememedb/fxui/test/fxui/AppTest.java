80d3fa63c1b4428078a779a81c03ad852852f334
hannathe <hannathe@stud.ntnu.no>
2020-10-07 10:40:11 +0200

    Made some more tests for the UI and added surefire dependency

--- mememedb/fxui/test/fxui/AppTest.java
+++ mememedb/fxui/test/fxui/AppTest.java
@@ -1,40 +1,89 @@
-// package it1901.mememedb.fxui;
+package fxui;
 
-// import org.junit.jupiter.api.Assertions;
-// import org.junit.jupiter.api.Test;
-// import org.testfx.framework.junit5.ApplicationTest;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Parent;
-// import javafx.scene.Scene;
-// import javafx.stage.Stage;
-// import javafx.scene.control.Button;
-// import javafx.scene.layout.VBox;
-// import javafx.scene.layout.HBox;
-// import javafx.scene.Node;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.testfx.framework.junit5.ApplicationTest;
 
-// import javafx.collections.ObservableList;
+import java.io.File;
 
-// import it1901.mememedb.fxui.AppController;
 
-// public class AppTest extends ApplicationTest {
-//   private Parent parent;
-//   private AppController controller;
+public class AppTest extends ApplicationTest {
+    private Parent parent;
+    private AppController controller;
 
-//   @Override
-//   public void start(final Stage stage) throws Exception {
-//     final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("App.fxml"));
-//     parent = fxmlLoader.load();
-//     controller = fxmlLoader.getController();
-//     stage.setScene(new Scene(parent));
-//     stage.show();
-//   }
+    @Mock
+    FileChooser fileChooser = Mockito.mock(FileChooser.class);
 
-//   @Test
-//   public void testButtons() {
-//     final Button logoutButton = (Button) parent.lookup("#logout");
-//     final Button addContent = (Button) parent.lookup("#addContent");
-//     clickOn(addContent);
-//     clickOn(logoutButton);
-//   }
+    @Override
+    public void start(final Stage stage) throws Exception {
+        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ui/App.fxml"));
+        parent = fxmlLoader.load();
+        controller = fxmlLoader.getController();
+        stage.setScene(new Scene(parent));
+        stage.show();
+    }
 
-// }
+    @Test
+    public void testButtons() {
+        final Button logoutButton = (Button) parent.lookup("#logout");
+        final Button addContent = (Button) parent.lookup("#addContent");
+        final Button registerClick = (Button) parent.lookup("#registerClick");
+        final Button browseButton = (Button) parent.lookup("browsebutton");
+        clickOn(addContent);
+        clickOn(logoutButton);
+        clickOn(registerClick);
+    }
+
+
+    @Test
+    public void testTextFields(){
+        //Browser
+        final TextField inputField = (TextField) parent.lookup("inputTextField");
+        //Register
+        final TextField nameTextField = (TextField) parent.lookup("nameTextField");
+        final TextField emailTextField = (TextField) parent.lookup("emailTextField");
+        final TextField usernameTextField = (TextField) parent.lookup("usernameTextField");
+        final PasswordField passwordTextField = (PasswordField) parent.lookup("passwordTextField");
+        //Login
+        final TextField loginUsername = (TextField) parent.lookup("loginUsername");
+        final PasswordField loginPasswordText = (PasswordField) parent.lookup("loginPassWordText");
+        clickOn(inputField);
+        write("This is a caption");
+
+
+    }
+
+
+
+
+
+    @Test
+    public void testAddContent(){
+        final TextField inputField = (TextField) parent.lookup("inputTextField");
+        final Button browseButton = (Button) parent.lookup("browsebutton");
+
+        clickOn(browseButton);
+        File inputFile = new File(getClass().getClassLoader().getResource("pangolin.png").getPath());
+
+    }
+
+
+    @Test
+    public void testSwitchLoginScenes(){
+        final Button registerClick = (Button) parent.lookup("#registerClick");
+        clickOn(registerClick);
+
+    }
+
+
+
+}
