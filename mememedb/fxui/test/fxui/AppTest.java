d19a7ecbaae93962dc359346e59e61db5fb9a9c3
Magne Halvorsen <maghalv@stud.ntnu.no>
2020-10-09 17:48:30 +0200

    (#23) Checked style on fxui test

--- mememedb/fxui/test/fxui/AppTest.java
+++ mememedb/fxui/test/fxui/AppTest.java
@@ -1,89 +1,59 @@
 package fxui;
 
+import static org.mockito.Mockito.*;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-
-import static org.mockito.Mockito.*;
 import org.testfx.framework.junit5.ApplicationTest;
 
-import java.io.File;
-
-
 public class AppTest extends ApplicationTest {
-    
-    private Parent parent;
-    private AppController controller;
-    private LoginController loginController;
-    private BrowserController browserController;
 
-  
+  private Parent parent;
+  private AppController controller;
+  private LoginController loginController;
+  private BrowserController browserController;
 
-    @Override
-    public void start(final Stage stage) throws Exception {
-        final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("App.fxml"));
-        parent = fxmlLoader.load();
-        controller = fxmlLoader.getController();
-        stage.setScene(new Scene(parent));
-        stage.show();
-    }
+  @Override
+  public void start(final Stage stage) throws Exception {
+    final FXMLLoader fxmlLoader =
+        new FXMLLoader(getClass().getClassLoader().getResource("App.fxml"));
+    parent = fxmlLoader.load();
+    controller = fxmlLoader.getController();
+    stage.setScene(new Scene(parent));
+    stage.show();
+  }
 
-    public void register(){
-        final Button registerButton = (Button) parent.lookup("#registerButton");
-        clickOn(registerButton);
+  public void register() {
+    final Button registerButton = (Button) parent.lookup("#registerButton");
+    clickOn(registerButton);
 
-        final TextField nameTextField = (TextField) parent.lookup("#nameTextField");
-        final TextField emailTextField = (TextField) parent.lookup("#emailTextField");
-        final TextField usernameTextField = (TextField) parent.lookup("#usernameTextField");
-        final PasswordField passwordTextField = (PasswordField) parent.lookup("#passwordTextField");
-        
-        final Button createUserButton = (Button) parent.lookup("#createUserButton");
+    final TextField nameTextField = (TextField) parent.lookup("#nameTextField");
+    final TextField emailTextField = (TextField) parent.lookup("#emailTextField");
+    final TextField usernameTextField = (TextField) parent.lookup("#usernameTextField");
+    final PasswordField passwordTextField = (PasswordField) parent.lookup("#passwordTextField");
 
+    final Button createUserButton = (Button) parent.lookup("#createUserButton");
 
-        clickOn(nameTextField);
-        write("John Doe");
-        clickOn(emailTextField);
-        write("johndoe@example.com");
-        clickOn(usernameTextField);
-        write("DoeMan69");
-        clickOn(passwordTextField);
-        write("iLoveMyMom123");
+    clickOn(nameTextField);
+    write("John Doe");
+    clickOn(emailTextField);
+    write("johndoe@example.com");
+    clickOn(usernameTextField);
+    write("DoeMan69");
+    clickOn(passwordTextField);
+    write("iLoveMyMom123");
 
-        clickOn(createUserButton);
-    }
+    clickOn(createUserButton);
+  }
 
-
-    @Test
-    public void registerTest(){
-        register();
-    }
-/*
-    @Test
-    public void testAddFile(){
-      register();
-      final Button browseButton = (Button) parent.lookup("#browseButton");
-      final TextField inputTextField = (TextField) parent.lookup("#inputTextField");
-      
-      
-      browserController.setImg("pangolin.jpg");
-      File inputFile = new File(getClass().getClassLoader().getResource("pangolin.jpg").getPath());
-      when(fileChooser.showOpenDialog(this.targetWindow())).thenReturn(inputFile);
-
-      clickOn(inputTextField);
-      write("This is a very nice animal");
-        
-    }
-
-    */
-
-
-
+  @Test
+  public void registerTest() {
+    register();
+  }
 }
