a36ce93d081d76a203eda35c377d8d8be9b7b911
Jostein Bakkevig <jostbak@stud.ntnu.no>
2020-10-10 15:54:34 +0000

    (#19) Fixed non-functional database test

--- mememedb/core/test/core/datastructures/DatabaseTest.java
+++ mememedb/core/test/core/datastructures/DatabaseTest.java
@@ -1,22 +1,53 @@
 package core.datastructures;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
+import core.io.IO;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 public class DatabaseTest {
 
-  /* FIXME: Please fix this test*/
-  /*
   List<User> testData =
       new ArrayList<User>(
           Arrays.asList(
               new User(1, "Bert Johnson", "berjon29", "bert@johnson.com"),
               new User(2, "Joe Mama", "jomama", "joe@mama.com")));
+  
+
+  /**
+   * Returns a mock database, always returning an empty list. 
+   * Used by mocked IO for passing content to database for testing purposes.
+   * 
+   * @return The mocked Database
+   */
+  public Database getEmptyMock(){
+    Database mockDb = mock(Database.class);
+    doReturn(new ArrayList<User>()).when(mockDb).getUsers();
+    return mockDb;
+  }
+
+  /**
+   * Returns a mock database, always returning the example testData. 
+   * Used by mocked IO for passing content to database for testing purposes.
+   * 
+   * @return The mocked Database
+   */
+  public Database getFilledMock(){
+    Database mockDb = mock(Database.class);
+    doReturn(testData).when(mockDb).getUsers();
+    return mockDb;
+  }
 
   @Test
   public void TestConstructDatabase() {
     final IO mockIO = mock(IO.class);
     // test with empty dataset
-    doReturn(new ArrayList<User>()).when(mockIO).getUserList();
+    doReturn(getEmptyMock()).when(mockIO).getDatabase();
     final Database databaseEmpty = new Database(mockIO);
     assertEquals(
         databaseEmpty.getUsers(),
@@ -24,7 +55,7 @@ public class DatabaseTest {
         "Database is not empty with empty construction");
 
     // test with already created dataset
-    doReturn(testData).when(mockIO).getUserList();
+    doReturn(getFilledMock()).when(mockIO).getDatabase();
     final Database databaseFull = new Database(mockIO);
     assertEquals(
         testData,
@@ -35,19 +66,19 @@ public class DatabaseTest {
   @Test
   public void TestNewUser() {
     final IO mockIO = mock(IO.class);
-    doReturn(new ArrayList<User>()).when(mockIO).getUserList();
+    doReturn(getEmptyMock()).when(mockIO).getDatabase();
     final Database database = new Database(mockIO);
     for (User user : testData) {
       database.saveUser(user);
     }
     assertEquals(database.getUsers(), testData, "Bug in user addition");
-    verify(mockIO, times(testData.size())).save(anyList());
+    verify(mockIO, times(testData.size())).saveDatabase(database);;
   }
 
   @Test
   public void TestPost() {
     final IO mockIO = mock(IO.class);
-    doReturn(testData).when(mockIO).getUserList();
+    doReturn(getFilledMock()).when(mockIO).getDatabase();
     Database database = new Database(mockIO);
     assertEquals(
         new ArrayList<Post>(),
@@ -73,13 +104,49 @@ public class DatabaseTest {
   @Test
   public void TestIdAssigner() {
     final IO mockIO = mock(IO.class);
+    doReturn(getEmptyMock()).when(mockIO).getDatabase();
     Database database = new Database(mockIO);
-    doReturn(testData).when(mockIO).getUserList();
     assertEquals(
-        1, database.getNewID(), "Error in id assignment, expected 1, got " + database.getNewID());
-    database.reload();
+        1, database.getNewId(), "Error in id assignment, expected 1, got " + database.getNewId());
+    doReturn(getFilledMock()).when(mockIO).getDatabase();
+    Database database2 = new Database(mockIO);
     assertEquals(
-        3, database.getNewID(), "Error in id assignment, expected 3, got " + database.getNewID());
+        3, database2.getNewId(), "Error in id assignment, expected 3, got " + database.getNewId());
+    assertEquals(
+        3, database2.getNewId(), "Error in id assignment, expected 3, got " + database.getNewId());
   }
-  */
+
+  @Test
+  public void TestgetImage() {
+    final IO mockIO = mock(IO.class);
+    doReturn(getEmptyMock()).when(mockIO).getDatabase();
+    Database database = new Database(mockIO);
+    doReturn(new File("/C:/TEST")).when(mockIO).getImageFromName("testImage");     
+    assertEquals("/C:/TEST",database.getImage("testImage").getAbsolutePath(),
+        "Error in /image fetching, database does not retain url");
+    verify(mockIO, times(1)).getImageFromName("testImage");
+  }
+
+  @Test
+  public void TestUsernameExists() {
+    final IO mockIO = mock(IO.class);
+    doReturn(getFilledMock()).when(mockIO).getDatabase();
+    Database database = new Database(mockIO);
+    assertEquals(false, database.usernameExists("notARealUser"), 
+        "Error in username availability, username should not exist yet");
+    assertEquals(true, database.usernameExists("berjon29"), 
+        "Error in username availability, username should already exist");
+  }
+
+  @Test
+  public void TestLogin() {
+    final IO mockIO = mock(IO.class);
+    testData.get(0).setPassword("asafepassword");
+    doReturn(getFilledMock()).when(mockIO).getDatabase();
+    Database database = new Database(mockIO);
+    assertEquals(testData.get(0), database.tryLogin("berjon29", "asafepassword"), 
+        "Error in login method, should return User");
+    assertEquals(null, database.tryLogin("berjon29", "awrongpassword"), 
+        "Error in login method, should fail with incorrect input");
+  }
 }
