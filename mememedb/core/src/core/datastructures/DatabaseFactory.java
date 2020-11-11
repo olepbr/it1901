149dab0b73012d35251004c39551bee3dee22c4c
Ole Peder Brandtzæg <olebra@stud.ntnu.no>
2020-11-11 18:33:05 +0000

    (#35) Implemented newUser method in RestDatabase, added java.net.http requirement to core module-info

--- mememedb/core/src/core/datastructures/DatabaseFactory.java
+++ mememedb/core/src/core/datastructures/DatabaseFactory.java
@@ -5,8 +5,9 @@ import core.io.LocalIO;
 public class DatabaseFactory {
 
   public DatabaseInterface getDatabase(String databaseType) {
-    if (databaseType.equalsIgnoreCase("rest"))
-      return new RestDatabase();
+    if (databaseType.equalsIgnoreCase("rest")) {
+        return new RestDatabase("http://localhost:8000");
+    }
     else if (databaseType.equalsIgnoreCase("local")) {
       return new LocalDatabase(new LocalIO());
     }
--- mememedb/core/src/core/datastructures/RestDatabase.java
+++ mememedb/core/src/core/datastructures/RestDatabase.java
@@ -1,10 +1,28 @@
 package core.datastructures;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import core.json.MememeModule;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.util.Collection;
 import java.util.Map;
 
 public class RestDatabase implements DatabaseInterface {
+
+  private final String endpointBaseUriString;
+
+  private ObjectMapper mapper;
+
+  public RestDatabase(String endpointBaseUriString) {
+    this.endpointBaseUriString = endpointBaseUriString;
+    this.mapper = new ObjectMapper().registerModule(new MememeModule());
+  }
 
   @Override
   public void newComment(String text, String owner, String postUUID) {
@@ -20,8 +38,18 @@ public class RestDatabase implements DatabaseInterface {
 
   @Override
   public void newUser(String name, String nickname, String email, String password) {
-    // TODO Auto-generated method stub
-
+    User user = new User(name, nickname, email, password);
+    try {
+      HttpRequest request = HttpRequest.newBuilder(new URI(endpointBaseUriString + "/user"))
+          .header("Accept", "application/json").POST(BodyPublishers.ofString(mapper.writeValueAsString(user))).build();
+      final HttpResponse<String> response =
+          HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+      System.out.println(response.body());
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    } catch (IOException | InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
@@ -60,4 +88,27 @@ public class RestDatabase implements DatabaseInterface {
     return false;
   }
 
+  @Override
+  public Comment getComment(String commentUUID, String postUUID) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Post getPost(String postUUID) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public User getUser(String nickname) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Collection<Comment> getComments(String postUUID) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 }
