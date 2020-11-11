48ea0f95ae31eb4f7fedaa2f4cd655f58418a14a
Ole Peder Brandtzæg <olebra@stud.ntnu.no>
2020-11-11 19:33:00 +0000

    (#35) Implemented methods utilising POST requests, added private requestSender method, formatted DatabaseInterface

--- mememedb/core/src/core/datastructures/DatabaseInterface.java
+++ mememedb/core/src/core/datastructures/DatabaseInterface.java
@@ -6,17 +6,29 @@ import java.util.Collection;
 import java.util.Map;
 
 public interface DatabaseInterface {
-	void newComment(String text, String owner, String postUUID);
-	void newPost(String owner, String caption, File image) throws IOException;
+  void newComment(String text, String owner, String postUUID);
+
+  void newPost(String owner, String caption, File image) throws IOException;
+
   void newUser(String name, String nickname, String email, String password);
+
   Comment getComment(String commentUUID, String postUUID);
+
   Post getPost(String postUUID);
+
   User getUser(String nickname);
-	Collection<User> getUsers();
-	Map<String, User> getUserMap();
-	Collection<Post> getPosts();
+
+  Collection<User> getUsers();
+
+  Map<String, User> getUserMap();
+
+  Collection<Post> getPosts();
+
   Map<String, Post> getPostMap();
+
   Collection<Comment> getComments(String postUUID);
-	User tryLogin(String username, String password);
-	boolean usernameExists(String username);
+
+  User tryLogin(String username, String password);
+
+  boolean usernameExists(String username);
 }
--- mememedb/core/src/core/datastructures/RestDatabase.java
+++ mememedb/core/src/core/datastructures/RestDatabase.java
@@ -8,8 +8,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
 import java.util.Collection;
 import java.util.Map;
 
@@ -24,32 +24,57 @@ public class RestDatabase implements DatabaseInterface {
     this.mapper = new ObjectMapper().registerModule(new MememeModule());
   }
 
-  @Override
-  public void newComment(String text, String owner, String postUUID) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void newPost(String owner, String caption, File image) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void newUser(String name, String nickname, String email, String password) {
-    User user = new User(name, nickname, email, password);
+  private void requestSender(String path, Object o, String type) {
     try {
-      HttpRequest request = HttpRequest.newBuilder(new URI(endpointBaseUriString + "/user"))
-          .header("Accept", "application/json").POST(BodyPublishers.ofString(mapper.writeValueAsString(user))).build();
-      final HttpResponse<String> response =
-          HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
-      System.out.println(response.body());
+      if (type.equalsIgnoreCase("POST")) {
+        HttpRequest request = HttpRequest.newBuilder(new URI(endpointBaseUriString + path))
+            .header("Accept", "application/json")
+            .POST(BodyPublishers.ofString(mapper.writeValueAsString(o)))
+            .build();
+        final HttpResponse<String> response =
+            HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+      } else if (type.equalsIgnoreCase("PUT")) {
+        HttpRequest request = HttpRequest.newBuilder(new URI(endpointBaseUriString + path))
+            .header("Accept", "application/json")
+            .PUT(BodyPublishers.ofString(mapper.writeValueAsString(o)))
+            .build();
+        final HttpResponse<String> response =
+            HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+      } else if (type.equalsIgnoreCase("DELETE")) {
+        HttpRequest request = HttpRequest.newBuilder(new URI(endpointBaseUriString + path))
+            .header("Accept", "application/json").DELETE().build();
+        final HttpResponse<String> response =
+            HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+      } else {
+        throw new IllegalArgumentException("Invalid request type");
+      }
     } catch (URISyntaxException e) {
       e.printStackTrace();
     } catch (IOException | InterruptedException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  @Override
+  public void newComment(String text, String owner, String postUuid) {
+    requestSender("/post/" + postUuid + "/comment", new Comment(owner, text), "POST");
+  }
+
+  @Override
+  public void newPost(String owner, String caption, File image) {
+    try {
+      requestSender("/post", new Post(owner, caption, image), "POST");
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void newUser(String name, String nickname, String email, String password) {
+    requestSender("/user", new User(name, nickname, email, password), "POST");
   }
 
   @Override
@@ -89,13 +114,13 @@ public class RestDatabase implements DatabaseInterface {
   }
 
   @Override
-  public Comment getComment(String commentUUID, String postUUID) {
+  public Comment getComment(String commentUuid, String postUuid) {
     // TODO Auto-generated method stub
     return null;
   }
 
   @Override
-  public Post getPost(String postUUID) {
+  public Post getPost(String postUuid) {
     // TODO Auto-generated method stub
     return null;
   }
@@ -107,7 +132,7 @@ public class RestDatabase implements DatabaseInterface {
   }
 
   @Override
-  public Collection<Comment> getComments(String postUUID) {
+  public Collection<Comment> getComments(String postUuid) {
     // TODO Auto-generated method stub
     return null;
   }
