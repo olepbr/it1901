d3c473a4e75a2980c3f8e24f7ca2d7f20134246e
Ole Peder Brandtzæg <olebra@stud.ntnu.no>
2020-11-16 15:03:44 +0000

    (#35) Implemented remaining getters in RestDatabase

--- mememedb/core/src/core/datastructures/RestDatabase.java
+++ mememedb/core/src/core/datastructures/RestDatabase.java
@@ -1,5 +1,7 @@
 package core.datastructures;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import core.json.MememeModule;
 import java.io.File;
@@ -10,9 +12,17 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+/**
+ * Provides a class implementing the Database interface that utilises the REST API to acquire data.
+ *
+ * @author Ole Peder Brandtzæg
+ */
 public class RestDatabase implements DatabaseInterface {
 
   private final String endpointBaseUriString;
@@ -28,25 +38,17 @@ public class RestDatabase implements DatabaseInterface {
     try {
       HttpRequest request;
       if (type.equalsIgnoreCase("POST")) {
-        request = HttpRequest.newBuilder(new URI(endpointBaseUriString + path))
-            .header("Accept", "application/json")
-            .POST(BodyPublishers.ofString(mapper.writeValueAsString(o)))
-            .build();
+        request = HttpRequest.newBuilder(new URI(endpointBaseUriString + path)).header("Accept", "application/json")
+            .POST(BodyPublishers.ofString(mapper.writeValueAsString(o))).build();
       } else if (type.equalsIgnoreCase("PUT")) {
-        request = HttpRequest.newBuilder(new URI(endpointBaseUriString + path))
-            .header("Accept", "application/json")
-            .PUT(BodyPublishers.ofString(mapper.writeValueAsString(o)))
-            .build();
+        request = HttpRequest.newBuilder(new URI(endpointBaseUriString + path)).header("Accept", "application/json")
+            .PUT(BodyPublishers.ofString(mapper.writeValueAsString(o))).build();
       } else if (type.equalsIgnoreCase("DELETE")) {
-        request = HttpRequest.newBuilder(new URI(endpointBaseUriString + path))
-            .header("Accept", "application/json")
-            .DELETE()
-            .build();
+        request = HttpRequest.newBuilder(new URI(endpointBaseUriString + path)).header("Accept", "application/json")
+            .DELETE().build();
       } else if (type.equalsIgnoreCase("GET")) {
-        request = HttpRequest.newBuilder(new URI(endpointBaseUriString + path))
-            .header("Accept", "application/json")
-            .GET()
-            .build();
+        request = HttpRequest.newBuilder(new URI(endpointBaseUriString + path)).header("Accept", "application/json")
+            .GET().build();
       } else {
         throw new IllegalArgumentException("Invalid request type");
       }
@@ -60,8 +62,8 @@ public class RestDatabase implements DatabaseInterface {
   }
 
   @Override
-  public void newComment(String text, String owner, String postUuid) {
-    requestHandler("/post/" + postUuid + "/comment", new Comment(owner, text), "POST");
+  public void newComment(String text, String owner, String postUUID) {
+    requestHandler("/post/" + postUUID + "/comment", new Comment(owner, text), "POST");
   }
 
   @Override
@@ -74,67 +76,117 @@ public class RestDatabase implements DatabaseInterface {
   }
 
   @Override
+  public void newPost(String owner, String caption, String imageData) {
+    requestHandler("/post", new Post(owner, caption, imageData), "POST");
+  }
+
+  @Override
   public void newUser(String name, String nickname, String email, String password) {
     requestHandler("/user", new User(name, nickname, email, password), "POST");
   }
 
   @Override
   public Collection<User> getUsers() {
-    // TODO Auto-generated method stub
-    return null;
+    Collection<User> userCollection = null;
+    try {
+      userCollection =
+          mapper.readValue(requestHandler("/user", null, "GET").body(), new TypeReference<Collection<User>>() {
+          });
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+    return userCollection;
   }
 
   @Override
   public Map<String, User> getUserMap() {
-    // TODO Auto-generated method stub
-    return null;
+    Collection<User> userCollection = getUsers();
+    return userCollection.stream().collect(Collectors.toMap(User::getNickname, Function.identity()));
   }
 
   @Override
   public Collection<Post> getPosts() {
-    // TODO Auto-generated method stub
-    return null;
+    Collection<Post> postCollection = null;
+    try {
+      postCollection =
+          mapper.readValue(requestHandler("/post", null, "GET").body(), new TypeReference<Collection<Post>>() {
+          });
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+    return postCollection;
   }
 
   @Override
   public Map<String, Post> getPostMap() {
-    // TODO Auto-generated method stub
-    return null;
+    Collection<Post> postCollection = getPosts();
+    return postCollection.stream().collect(Collectors.toMap(Post::getUUID, Function.identity()));
   }
 
   @Override
   public User tryLogin(String username, String password) {
-    // TODO Auto-generated method stub
+    User user = getUserMap().getOrDefault(username, null);
+    if (user != null && user.getPassword().equals(User.hashPassword(password))) {
+      return user;
+    }
     return null;
   }
 
   @Override
   public boolean usernameExists(String username) {
-    // TODO Auto-generated method stub
-    return false;
+    return getUserMap().containsKey(username);
   }
 
   @Override
-  public Comment getComment(String commentUuid, String postUuid) {
-    // TODO Auto-generated method stub
-    return null;
+  public Comment getComment(String commentUUID, String postUUID) {
+    Comment comment = null;
+    try {
+      comment = mapper
+          .readValue(requestHandler("/post/:" + postUUID + "/comment/:" + commentUUID, null, "GET")
+          .body(), Comment.class);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+    return comment;
   }
 
   @Override
-  public Post getPost(String postUuid) {
-    // TODO Auto-generated method stub
-    return null;
+  public Post getPost(String postUUID) {
+    Post post = null;
+    try {
+      post = mapper
+          .readValue(requestHandler("/post/:" + postUUID, null, "GET")
+          .body(), Post.class);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+    return post;
   }
 
   @Override
   public User getUser(String nickname) {
-    // TODO Auto-generated method stub
-    return null;
+    User user = null;
+    try {
+      user= mapper
+          .readValue(requestHandler("/user/:" + nickname, null, "GET")
+          .body(), User.class);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+    return user;
   }
 
   @Override
-  public Collection<Comment> getComments(String postUuid) {
-    // TODO Auto-generated method stub
-    return null;
+  public Collection<Comment> getComments(String postUUID) {
+    Collection<Comment> comments = null;
+    try {
+      comments =
+          mapper
+          .readValue(requestHandler("/post/:" + postUUID + "/comment", null, "GET")
+          .body(), new TypeReference<Collection<Comment>>() {});
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+    return comments;
   }
 }
